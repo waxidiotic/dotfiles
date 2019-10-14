b9f26227840c19e2ee98d066db982aa1d1195341
Alex Bussey <abussey@jwplayer.com>
2019-10-13 21:33:56 -0400

    Hopefully finished

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 echo "Setting up your Mac..."
 
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -18,19 +21,23 @@ brew bundle
 # chsh -s $(which zsh)
 
 # Install global NPM packages
-npm install --global yarn pure-prompt
+npm install --global pure-prompt
 
 # Create a Repos directory
 # This is a default directory for git repositories
 mkdir $HOME/Repos
 
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
-# rm -rf $HOME/.zshrc
-# ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
-# TODO: .zshrc aliases
-# TODO: .zshrc
-# TODO: .profile
-# TODO: code
+rm -rf $HOME/.zshrc
+ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+
+# Setup Hyper
+rm -rf $HOME/.hyper.js
+cp $HOME/.dotfiles/.hyper.js $HOME
+
+# Setup gitconfig and global ignores
+rm -rf $HOME/.gitconfig $HOME/.gitignore_global
+cp $HOME/.dotfiles/.gitconfig $HOME/.dotfiles/.gitignore_global $HOME
 
 # Set macOS preferences
 # We will run this last because this will reload the shell
